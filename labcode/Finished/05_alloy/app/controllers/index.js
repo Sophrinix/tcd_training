1db69ee654314274f4147c00e76764204b044352
Andrew McElroy <sophrinix@gmail.com>
2014-05-13 07:32:35 -0500

    Update index.js
    this code crashes on Android. line 6 should actually read: $.index.addEventListener 
    and not $.tabGroup....
    
    
    This change needs to be applied to all the labs.

--- labcode/Finished/05_alloy/app/controllers/index.js
+++ labcode/Finished/05_alloy/app/controllers/index.js
@@ -3,7 +3,7 @@ $.index.open();
 
 if(OS_ANDROID) {
     // Add the app's name to the Android action bar
-    $.tabGroup.addEventListener('open', function(e) {
+    $.index.addEventListener('open', function(e) {
         var activity = $.tabGroup.getActivity(); 
         if (activity != undefined && activity.actionBar != undefined) {
             // set the title to the app name defined in tiapp.xml
